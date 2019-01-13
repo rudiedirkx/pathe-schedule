2f5f739884fede2c9797cbf513af5f4bcee88dee
Rudie Dirkx <github@hotblocks.nl>
2019-01-13 19:59:33 +0100

    +cfg db location

--- env.php.orig.php
+++ env.php.orig.php
@@ -2,5 +2,6 @@
 
 define('PATHE_OBJECT_STORE_URL', 'https://store.example.com/');
 define('PATHE_OBJECT_STORE', 'foo');
+define('PATHE_DB_FILE', __DIR__ . '/db/pathe.sqlite3');
 define('PATHE_DOWNLOAD_DIR', __DIR__ . '/cache');
 define('PATHE_DOWNLOAD_TTL', 300);
--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -3,6 +3,6 @@
 require __DIR__ . '/env.php';
 require __DIR__ . '/vendor/autoload.php';
 
-$db = db_sqlite::open(array('database' => __DIR__ . '/db/pathe.sqlite3'));
+$db = db_sqlite::open(array('database' => PATHE_DB_FILE));
 $db->ensureSchema(require 'inc.db-schema.php');
 db_generic_model::$_db = $db;
