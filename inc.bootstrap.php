c4aaefd3164f23d866876f042649adedd72f00f0
Rudie Dirkx <github@hotblocks.nl>
2026-05-25 16:04:28 +0200

    cookie+

--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -7,4 +7,12 @@ $db = db_sqlite::open(array('database' => PATHE_DB_FILE));
 db_generic_model::$_db = $db;
 $db->ensureSchema(require 'inc.db-schema.php');
 
+session_name('patheschedulesession');
+session_set_cookie_params([
+	'lifetime' => 86400 * 60,
+	'path' => '/',
+]);
 session_start();
+if (rand(1, 5) == 1) {
+	session_regenerate_id(true);
+}
